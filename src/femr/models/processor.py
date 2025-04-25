810b8cad8e433eefd11011371e34b713b9cb3c4c
bschilder <brian_schilder@alumni.brown.edu>
2025-04-25 01:08:45 -0400

    Update femr for MEDSv1. Add helper functions

--- src/femr/models/processor.py
+++ src/femr/models/processor.py
@@ -114,7 +114,10 @@ class BatchCreator:
         if self.task is not None:
             self.task.start_batch()
 
-    def add_patient(self, patient: meds.Patient, offset: int = 0, max_length: Optional[int] = None):
+    def add_patient(self, 
+                    patient: datasets.Dataset,
+                     offset: int = 0, 
+                     max_length: Optional[int] = None):
         """Add a patient to the current batch.
 
         Note that the two optional parameters are used to add a subset of a patient to a batch.
@@ -128,6 +131,7 @@ class BatchCreator:
             max_length: The maximum length of the batch sequence. There is no max when left at None.
 
         """
+        codes_seen_today = set()
         current_date = None
         last_time = None
 
@@ -163,15 +167,31 @@ class BatchCreator:
         birth = femr.pat_utils.get_patient_birthdate(patient)
         self.tokenizer.start_patient()
 
-        for event in patient["events"]:
+        events_map = femr.pat_utils.get_event_indices(patient)
+
+        for event_time, event_idx in events_map.items():
+            
             # We want to avoid duplicate codes in the same day, so we maintain codes_seen_today
-            if event["time"].date() != current_date:
-                current_date = event["time"].date()
+            if event_time is not None and event_time.date() != current_date:
+                current_date = event_time.date()
                 codes_seen_today = set()
 
-            for measurement in event["measurements"]:
+
+            # Measurements are lists of dictionaries with a 'code' key
+            # Each item in the list represents an event, during which there can be multiple measurements (multiple dictionaries with a 'code' key)
+            event_measurements = femr.pat_utils.filter_dataset(patient, {"table_name": "measurement",
+                                                                         "time": event_time})
+            
+            if len(event_measurements) == 0:
+                raise ValueError(f"No measurements found for patient {patient['subject_id'][0]}")
+            
+            for measurement in event_measurements:
+
                 # Get features and weights for the current event
-                features, weights = self.tokenizer.get_feature_codes(event["time"], measurement)
+                if event_time is None:
+                    raise ValueError(f"No time found for patient {patient['subject_id'][0]}")
+                
+                features, weights = self.tokenizer.get_feature_codes(event_time, measurement)
 
                 # Ignore events with no features
                 if len(features) == 0:
@@ -186,7 +206,7 @@ class BatchCreator:
                 if (self.task is not None) and (last_time is not None):
                     # Now we have to consider whether or not to have labels for this time step
                     # The add_event function returns how many labels to assign for this time
-                    num_added = self.task.add_event(last_time, event["time"], features)
+                    num_added = self.task.add_event(last_time, event_time, features)
                     for _ in range(num_added):
                         per_patient_label_indices.append(len(per_patient_ages) - 1)
 
@@ -199,11 +219,11 @@ class BatchCreator:
                     per_patient_hierarchical_weights.extend(weights)
                     per_patient_token_indices.append(len(per_patient_hierarchical_tokens))
 
-                per_patient_ages.append((event["time"] - birth) / datetime.timedelta(days=1))
-                per_patient_normalized_ages.append(self.tokenizer.normalize_age(event["time"] - birth))
-                per_patient_timestamps.append(event["time"].replace(tzinfo=datetime.timezone.utc).timestamp())
+                per_patient_ages.append((event_time - birth) / datetime.timedelta(days=1))
+                per_patient_normalized_ages.append(self.tokenizer.normalize_age(event_time - birth))
+                per_patient_timestamps.append(event_time.replace(tzinfo=datetime.timezone.utc).timestamp())
 
-                last_time = event["time"]
+                last_time = event_time
 
         if self.task is not None and last_time is not None:
             num_added = self.task.add_event(last_time, None, None)
@@ -224,7 +244,7 @@ class BatchCreator:
 
         # Let's add the constants first
         self.valid_tokens.extend([True] * length_to_add)
-        self.patient_ids.extend([patient["patient_id"]] * length_to_add)
+        self.patient_ids.extend([patient["subject_id"][0]] * length_to_add)
         self.offsets.append(offset)
         self.patient_lengths.append(length_to_add)
 
@@ -363,7 +383,7 @@ class FEMRBatchProcessor:
 
     def convert_patient(
         self,
-        patient: meds.Patient,
+        patient: datasets.Dataset,
         offset: int = 0,
         max_length: Optional[int] = None,
         tensor_type=None,
--- src/femr/pat_utils.py
+++ src/femr/pat_utils.py
@@ -1,11 +1,137 @@
 import datetime
-
 import meds
+import datasets
+import numpy as np
+from femr.model_utils import get_model_vocab
+
+### MEDS v1 version
+# def get_patient_birthdate(patient: meds.subject_id_dtype) -> datetime.datetime:
+#     for e in patient["events"]:
+#         for m in e["measurements"]:
+#             if m["code"] == meds.birth_code:
+#                 return e["time"]
+#     raise ValueError("Couldn't find patient birthdate -- Patient has no events " + str(patient["events"][:5]))
 
 
-def get_patient_birthdate(patient: meds.Patient) -> datetime.datetime:
-    for e in patient["events"]:
-        for m in e["measurements"]:
-            if m["code"] == meds.birth_code:
-                return e["time"]
-    raise ValueError("Couldn't find patient birthdate -- Patient has no events " + str(patient["events"][:5]))
+### MEDS v3 version
+def get_patient_birthdate(patient: datasets.Dataset) -> datetime.datetime:
+    """
+    Get patient birthdate from a flat dataset format.
+    
+    Args:
+        patient: A row from the dataset containing patient information
+        
+    Returns:
+        datetime.datetime: The patient's birthdate
+
+    Example:
+        >>> patient = MEDSV3_DATA.filter(lambda x: x['subject_id'] == 101)
+        >>> get_patient_birthdate(patient)
+    """
+    # Check if birthdate is directly available in the dataset
+    for i, code in enumerate(patient['code']):
+        if code == meds.birth_code:
+            return patient["time"][i] 
+    raise ValueError(f"Couldn't find patient birthdate for patient {patient['subject_id'][0]}")
+
+
+def get_table_indices(patient: datasets.Dataset, table_name: str) -> np.ndarray:
+    """
+    Get the indices of the rows in the dataset that correspond to a given table name.
+    """
+    return np.where(np.array(patient["table_name"]) == table_name)[0]
+
+def check_vocab_overlap(model_name: str,
+                        patient: datasets.Dataset,
+                        code_col: str = 'code'):
+    """
+    Check if the codes in the patient data are present in the model vocabulary.
+
+    Args:
+        model_name: The name of the model to check the vocabulary against.
+        patient: The patient dataset to check the vocabulary against.
+        code_col: The column name of the codes in the patient data.
+
+    Returns:
+        The overlap between the model vocabulary and the patient data.
+
+    Example:
+        >>> check_vocab_overlap(model_name="StanfordShahLab/clmbr-t-base",
+                                patient=MEDSV3_DATA,
+                                code_col="code")
+    """
+    model_codes = get_model_vocab(model_name=model_name, 
+                                  codes_only=True)
+
+    # First, we should check that at least some of the codes in the model are present in the MEDSV3_DATA
+    overlap = set(model_codes).intersection(set(patient[code_col]))
+    if len(overlap) == 0:
+        raise ValueError(f"No overlap between model vocabulary and patient data for model {model_name}")
+    else:
+        print(f"Overlap between model vocabulary and patient data for model {model_name}: {len(overlap)}")
+    return overlap
+
+def filter_dataset(dataset: datasets.Dataset,
+                   filter_dict: dict):
+    """
+    Filter a dataset.
+
+    Args:
+        dataset: The dataset to filter.
+        filter_dict: A dictionary of column names and values to filter by.
+
+    Returns:
+        The filtered dataset.
+
+    Example:
+        >>> filter_dataset(dataset=MEDSV3_DATA,
+                           filter_dict={"table_name": "measurement",
+                                       "code": "Visit/IP"})
+    """    
+    for key, value in filter_dict.items():
+        if not isinstance(value, list):
+            value = [value]
+        dataset = dataset.filter(lambda x: x[key] in value)
+    return dataset
+
+def filter_patients(dataset: datasets.Dataset,
+                    subject_ids: list[int] | int | str,
+                    subject_id_col: str = 'subject_id'):
+    """
+    Filter a dataset for a specific set of patient IDs.
+
+    Args:
+        dataset: The dataset to filter.
+        patient_ids: The list of patient IDs to filter for.
+        subject_id_col: The column name of the subject IDs in the dataset.
+
+    Returns:
+        The filtered dataset.
+    """
+    if isinstance(subject_ids, int):
+        subject_ids = [subject_ids]
+    
+    if isinstance(subject_ids, str):
+        subject_ids = [int(subject_ids)]
+
+    return filter_dataset(dataset=dataset,
+                          filter_dict={subject_id_col: subject_ids})
+
+def get_event_indices(dataset: datasets.Dataset):
+    """
+    Get the indices of the rows in the dataset that correspond to a given event (i.e. a unique timestamp).
+
+    Args:
+        dataset: The dataset to get the event indices from.
+
+    Returns:
+        A dictionary of times and their corresponding indices.
+    """
+    time_indices = {}
+    for i, time in enumerate(dataset['time']):
+        if time is not None:
+            if time not in time_indices:
+                time_indices[time] = []
+            time_indices[time].append(i)
+
+    return time_indices
